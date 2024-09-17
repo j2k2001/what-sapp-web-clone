711c66578a41aeeeba5b7cba1633a78472f6a23e
j2k <j2k.code@gmail.com>
2024-09-17 10:19:05 +0530

    added the persistent and scrolling functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Leftpane,Rightpane} from './containers';
 import './App.css';
 import { useState } from 'react';
 
 const App = () => {
+  //implementing the fetching functions from the local storage
+  //we will persist only messages and connections over the page refreshes
+
+  const getInitialMessages = () => {
+    const savedMessages = localStorage.getItem("messages");
+    return savedMessages ? JSON.parse(savedMessages) : [] ;
+  }
+
+  const getInitialConnections = () => {
+    const savedConnections = localStorage.getItem("connections");
+    return savedConnections ? JSON.parse(savedConnections) : [];
+  }
+
   const [selectedConvId,setSelectedConvId] = useState(null);
-  const [messages,setMessages] = useState([]);
-  const [connections,setConnections] = useState([]);
+  const [messages,setMessages] = useState(getInitialMessages);
+  const [connections,setConnections] = useState(getInitialConnections);
+
+  useEffect(() => {
+    localStorage.setItem("messages",JSON.stringify(messages));
+  },[messages]);
+
+  useEffect(() => {
+    localStorage.setItem("connections",JSON.stringify(connections));
+  },[connections]);
+  
   return (
     <div className='app'>
       <Leftpane  setSelectedConvId={setSelectedConvId} connections={connections} setConnections={setConnections}></Leftpane>
