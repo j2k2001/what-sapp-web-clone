d9e641558eafd41feb26b87a30087987bef22d24
j2k <j2k.code@gmail.com>
2024-09-24 16:57:03 +0530

    new chat modal is functional

--- src/components/Modalcomponent1/Modalcomponent1.jsx
+++ src/components/Modalcomponent1/Modalcomponent1.jsx
@@ -9,7 +9,7 @@ const Modalcomponent1 = ({placeholdertext,button1,button2,button1f,button2f}) =>
       <div className="modalcomponentinnercontainer">
         <input className='inputmessagecontainer' type="text" value={text} onChange={(e)=>{setText(e.target.value)}} placeholder={placeholdertext}/>
         <div className="buttoncontainerinmodal">
-          <button className="modalbutton" onClick={button1f}>{button1}</button>
+          <button className="modalbutton" onClick={()=>button1f(text)}>{button1}</button>
           <button className="modalbutton" onClick={button2f}>{button2}</button>
         </div>
       </div>
--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -6,13 +6,22 @@ import {Modalcomponent1} from '../../components';
 
 const Newchatbutton = ({setConnections}) => {
   const [newChatModalOpen,setNewChatModalOpen] = useState(false);
-  function handleclick(){
+  function handleClick(){
     setNewChatModalOpen(true);
+  }
+  function handleAdd(userInput){
+    if(userInput!==null){
+      setConnections((connections)=>[...connections,{id:Date.now(),name:userInput,profileImg:profileImg}])
+    }
+    setNewChatModalOpen(false);
+  }
+  function handleCancel(){
+    setNewChatModalOpen(false);
   }
   return (
     <>
-      {newChatModalOpen&&<Modalcomponent1 placeholdertext="New User" button1="Add" button2="Cancel" /> }
-      <button className="startnewchatbutton" onClick={handleclick}>New Chat</button>
+      {newChatModalOpen&&<Modalcomponent1 placeholdertext="New User" button1="Add" button2="Cancel" button1f={handleAdd} button2f={handleCancel}/> }
+      <button className="startnewchatbutton" onClick={handleClick}>New Chat</button>
     </>
   );
 }
